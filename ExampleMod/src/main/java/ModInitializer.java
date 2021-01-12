ab98fc20b9dea763c72a5dceb20238385555e18e
Derek Jass <jazzer36@gmail.com>
2021-01-12 09:13:01 -0600

    Following example code and ignoring build-related files.

--- ExampleMod/src/main/java/ModInitializer.java
+++ ExampleMod/src/main/java/ModInitializer.java
@@ -1,26 +1,21 @@
+import basemod.BaseMod;
+import basemod.interfaces.PostDrawSubscriber;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 
-//When all External Libraries are added from the pom.xml using maven the code can be uncommented.
+@SpireInitializer
+public class ModInitializer implements PostDrawSubscriber {
 
-//import basemod.BaseMod;
-//import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
-
-//@SpireInitializer
-public class ModInitializer {
-
-
-    public ModInitializer(){
-        //Use this for when you subscribe to any hooks offered by BaseMod.
-        //BaseMod.subscribe(this)
+    public ModInitializer() {
+        BaseMod.subscribe(this);
     }
 
-    //Used by @SpireInitializer
-    public static void initialize(){
-
-        //This creates an instance of our classes and gets our code going after BaseMod and ModTheSpire initialize.
+    public static void initialize() {
         ModInitializer modInitializer = new ModInitializer();
-
-        //Look at the BaseMod wiki for getting started. (Skip the decompiling part. It's not needed right now)
-
     }
 
+    @Override
+    public void receivePostDraw(AbstractCard card) {
+        System.out.println(card.name + " was drawn!");
+    }
 }
