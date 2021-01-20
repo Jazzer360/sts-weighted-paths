14ed2c02b8a109d467e87068a6afdabc8e2a519d
Derek Jass <jazzer36@gmail.com>
2021-01-20 09:46:55 -0600

    Improved handling of rendering.

---
+++ WeightedPaths/src/main/java/com/derekjass/sts/weightedpaths/patches/PreMapRoomNodeRenderPatch.java
@@ -0,0 +1,30 @@
+package com.derekjass.sts.weightedpaths.patches;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.derekjass.sts.weightedpaths.WeightedPaths;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.map.MapRoomNode;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SpirePatch(clz = MapRoomNode.class, method="render")
+public class PreMapRoomNodeRenderPatch {
+
+    private static final Logger logger = LogManager.getLogger(PreMapRoomNodeRenderPatch.class.getName());
+    private static final Color WEIGHT_COLOR = new Color(0x00_00_00_58);
+
+    @SpirePrefixPatch
+    public static void onPreMapRoomNodeRender(MapRoomNode room, SpriteBatch sb) {
+        if (WeightedPaths.roomValues.containsKey(room)) {
+            drawNodeValue(room, WeightedPaths.roomValues.get(room), sb);
+        }
+    }
+
+    public static void drawNodeValue(MapRoomNode room, float value, SpriteBatch sb) {
+        FontHelper.renderFont(sb, FontHelper.tipBodyFont, String.format("%.0f", value),
+                room.hb.cX + 20, room.hb.cY, WEIGHT_COLOR);
+    }
+}
