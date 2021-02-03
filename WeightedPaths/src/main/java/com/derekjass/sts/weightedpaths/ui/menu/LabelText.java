b67fe49d814ed406ab08048ae29529a7aced3491
Derek Jass <jazzer36@gmail.com>
2021-02-02 18:14:19 -0600

    Suppress parameter warning.

--- WeightedPaths/src/main/java/com/derekjass/sts/weightedpaths/ui/menu/LabelText.java
+++ WeightedPaths/src/main/java/com/derekjass/sts/weightedpaths/ui/menu/LabelText.java
@@ -9,7 +9,7 @@ public class LabelText implements Renderable {
     private final String label;
     private final float x, y;
 
-    LabelText(float x, float y, String label) {
+    LabelText(@SuppressWarnings("SameParameterValue") float x, float y, String label) {
         this.x = x;
         this.y = y;
         this.label = label;
